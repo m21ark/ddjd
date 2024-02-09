5c9bcb0394038ab30f5654aa40db748aea9a8a23
Marco André <marcoandre2103@gmail.com>
2024-02-09 21:11:35 +0000

    Fix

---
+++ TurboClash/Assets/PlayerKickBall.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKickBall : MonoBehaviour
+{
+    public int kickForce = 3; // force to kick the ball
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // if collision with player, kick the ball in the direction the player is facing
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2"){
+            // Get the ball and player's rigidbodies
+            Rigidbody ball = GameObject.FindWithTag("Ball").GetComponent<Rigidbody>();
+            Rigidbody player = collision.gameObject.GetComponent<Rigidbody>();
+
+            if(ball == null || player == null) return;
+
+            // Calculate the direction from the ball to the player
+            Vector3 direction = ball.transform.position - collision.gameObject.transform.position;
+            direction.Normalize();
+            
+            // Apply force to the ball, scaling it with the player's speed
+            float playerSpeed = player.velocity.magnitude;
+            ball.AddForce(direction * playerSpeed * kickForce, ForceMode.Impulse);
+        }
+    } 
+}
