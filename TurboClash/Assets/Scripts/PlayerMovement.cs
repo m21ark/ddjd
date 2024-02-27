1b19ef330bfcc87aeac08f95bde1c2f7650fec46
Marco André <marcoandre2103@gmail.com>
2024-02-27 19:15:57 +0000

    Crazy follow cam

--- TurboClash/Assets/Scripts/PlayerMovement.cs
+++ TurboClash/Assets/Scripts/PlayerMovement.cs
@@ -10,11 +10,14 @@ public class playerMovement : MonoBehaviour
     public float jump = 4f;
     public Camera playerCamera;
 
+    private GameObject ball;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ball = GameObject.FindGameObjectWithTag("Ball");
     }
+
 
     // Update is called once per frame
     void Update()
@@ -40,6 +43,54 @@ public class playerMovement : MonoBehaviour
     }
 
 
+
+    faceCamBall();
+
+
+    /* //Cam update
+    ball = GameObject.FindGameObjectWithTag("Ball");
+    Vector3 lookDirection = ball.transform.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+        playerCamera.transform.rotation = Quaternion.RotateTowards(playerCamera.transform.rotation, targetRotation, Time.deltaTime * 100f);
+
+        // Make camera go down and behind the player (in the direction of the ball)
+        Vector3 cameraOffset = -playerCamera.transform.forward * 5f + Vector3.up * 2f;
+        playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, transform.position + cameraOffset, Time.deltaTime * 5f); */
+    }
+
+    void faceCamBall(){
+
+    float rotationSpeed = 15f;
+    float distanceBehind = 3f;
+    float heightAbove = 1f;
+    float smoothTime = 0.5f;
+    Vector3 cameraVelocity = Vector3.zero;
+
+    // Update camera position to follow both player and ball
+    ball = GameObject.FindGameObjectWithTag("Ball");
+    if (ball != null)
+    {
+        Vector3 lookDirection = ball.transform.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+        playerCamera.transform.rotation = Quaternion.RotateTowards(playerCamera.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+
+        // Calculate the desired camera position
+        Vector3 targetPosition = transform.position - playerCamera.transform.forward * distanceBehind + Vector3.up * heightAbove;
+
+        // Smoothly move the camera towards the target position
+        playerCamera.transform.position = Vector3.SmoothDamp(playerCamera.transform.position, targetPosition, ref cameraVelocity, smoothTime);
+    }
+
+    }
+
+     void FixedUpdate()
+    {
+        // Move player based on input
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(inputHorizontal, 0.0f, inputVertical);
+        rb.AddForce(movement * speed);
     }
 
 
