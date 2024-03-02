ecd3590c07686d4be1fad03ff9d081dda9772756
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2024-03-02 18:21:58 +0000

    First camera aproach to changing direction of view

--- TurboClash/Assets/Scripts/PlayerMovement.cs
+++ TurboClash/Assets/Scripts/PlayerMovement.cs
@@ -23,67 +23,73 @@ public class playerMovement : MonoBehaviour
     void Update()
     {
 
-    bool isPlayer1 = gameObject.tag == "Player1";
+        bool isPlayer1 = gameObject.tag == "Player1";
 
-    // Jump P1
-    if (Input.GetKey(KeyCode.Space) && transform.position.y < 0.3f)
-        if (isPlayer1)
-            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+        // Jump P1
+        if (Input.GetKey(KeyCode.Space) && transform.position.y < 0.3f)
+            if (isPlayer1)
+                rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
 
-    // Jump P2
-    if (Input.GetKey(KeyCode.Return) && transform.position.y < 0.3f)
-        if (!isPlayer1)
-            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+        // Jump P2
+        if (Input.GetKey(KeyCode.Return) && transform.position.y < 0.3f)
+            if (!isPlayer1)
+                rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
 
-    // If the player is upside down, flip them back up
-    if (transform.rotation.eulerAngles.z > 90 && transform.rotation.eulerAngles.z < 270){
-        Debug.Log("Player is upside down");
-        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
-        rb.angularVelocity = Vector3.zero;
-    }
-
-
-
-    faceCamBall();
+        // If the player is upside down, flip them back up
+        if (transform.rotation.eulerAngles.z > 90 && transform.rotation.eulerAngles.z < 270)
+        {
+            Debug.Log("Player is upside down");
+            transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+            rb.angularVelocity = Vector3.zero;
+        }
 
 
-    /* //Cam update
-    ball = GameObject.FindGameObjectWithTag("Ball");
-    Vector3 lookDirection = ball.transform.position - transform.position;
-        Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
-        playerCamera.transform.rotation = Quaternion.RotateTowards(playerCamera.transform.rotation, targetRotation, Time.deltaTime * 100f);
 
-        // Make camera go down and behind the player (in the direction of the ball)
-        Vector3 cameraOffset = -playerCamera.transform.forward * 5f + Vector3.up * 2f;
-        playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, transform.position + cameraOffset, Time.deltaTime * 5f); */
-    }
+        faceCamBall();
 
-    void faceCamBall(){
 
-    float rotationSpeed = 15f;
-    float distanceBehind = 3f;
-    float heightAbove = 1f;
-    float smoothTime = 0.5f;
-    Vector3 cameraVelocity = Vector3.zero;
-
-    // Update camera position to follow both player and ball
-    ball = GameObject.FindGameObjectWithTag("Ball");
-    if (ball != null)
-    {
+        /* //Cam update
+        ball = GameObject.FindGameObjectWithTag("Ball");
         Vector3 lookDirection = ball.transform.position - transform.position;
-        Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
-        playerCamera.transform.rotation = Quaternion.RotateTowards(playerCamera.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+            Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+            playerCamera.transform.rotation = Quaternion.RotateTowards(playerCamera.transform.rotation, targetRotation, Time.deltaTime * 100f);
 
-        // Calculate the desired camera position
-        Vector3 targetPosition = transform.position - playerCamera.transform.forward * distanceBehind + Vector3.up * heightAbove;
-
-        // Smoothly move the camera towards the target position
-        playerCamera.transform.position = Vector3.SmoothDamp(playerCamera.transform.position, targetPosition, ref cameraVelocity, smoothTime);
+            // Make camera go down and behind the player (in the direction of the ball)
+            Vector3 cameraOffset = -playerCamera.transform.forward * 5f + Vector3.up * 2f;
+            playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, transform.position + cameraOffset, Time.deltaTime * 5f); */
     }
 
+    void faceCamBall()
+    {
+        float distanceBehindCar = 5f; 
+        float heightAboveCar = 2f; 
+
+        GameObject car = GameObject.FindGameObjectWithTag(gameObject.tag);
+        GameObject ball = GameObject.FindGameObjectWithTag("Ball");
+
+        if (car != null && ball != null)
+        {
+            Vector3 desiredPosition = car.transform.position - car.transform.forward * distanceBehindCar + Vector3.up * heightAboveCar;
+            Vector3 carToBallDirection = ball.transform.position - car.transform.position;
+
+            if (Vector3.Dot(car.transform.forward, carToBallDirection) < 0)
+            {
+                desiredPosition = car.transform.position + car.transform.forward * distanceBehindCar + Vector3.up * heightAboveCar;
+            }
+
+            Vector3 lookDirection = ball.transform.position - desiredPosition;
+            Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+
+            playerCamera.transform.position = desiredPosition;
+            playerCamera.transform.rotation = targetRotation;
+
+        }
     }
 
-     void FixedUpdate()
+
+
+
+    void FixedUpdate()
     {
         // Move player based on input
         float inputHorizontal = Input.GetAxis("Horizontal");
@@ -94,7 +100,8 @@ public class playerMovement : MonoBehaviour
     }
 
 
-    public void Move(float inputHorizontal, float inputVertical){
+    public void Move(float inputHorizontal, float inputVertical)
+    {
         // Get the forward and right vectors of the camera
         Vector3 cameraForward = playerCamera.transform.forward;
         Vector3 cameraRight = playerCamera.transform.right;
@@ -110,7 +117,7 @@ public class playerMovement : MonoBehaviour
         rb.velocity = moveDirection * speed;
     }
 
-  
+
 }
 
-   
+
