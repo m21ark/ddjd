4c56d8ecddec3d3955080e4a311e40a0d4dd6204
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2024-03-05 18:56:56 +0000

    lights

--- TurboClash/Assets/Scripts/PlayerMovement.cs
+++ TurboClash/Assets/Scripts/PlayerMovement.cs
@@ -67,6 +67,7 @@ public class playerMovement : MonoBehaviour
         }
 
         // faceCamBall();
+        
 
 
         /* //Cam update
@@ -80,21 +81,24 @@ public class playerMovement : MonoBehaviour
             playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, transform.position + cameraOffset, Time.deltaTime * 5f); */
     }
 
-    void faceCamBall()
+   
+    void faceCamBall() // ESTA SOLUÇAO FUNCIONA BEM
     {
-        float distanceBehindCar = 4f;
-        float heightAboveCar = 1f;
+        float distanceBehindCar = 5f; 
+        float heightAboveCar = 2f; 
 
         GameObject car = GameObject.FindGameObjectWithTag(gameObject.tag);
         GameObject ball = GameObject.FindGameObjectWithTag("Ball");
 
         if (car != null && ball != null)
         {
+            Vector3 desiredPosition = car.transform.position - car.transform.forward * distanceBehindCar + Vector3.up * heightAboveCar;
             Vector3 carToBallDirection = ball.transform.position - car.transform.position;
 
-            float dot = Mathf.Clamp(Vector3.Dot(car.transform.forward, carToBallDirection), -1.2f, 1.2f);
-
-            Vector3 desiredPosition = car.transform.position - car.transform.forward * distanceBehindCar * dot + Vector3.up * heightAboveCar;
+            if (Vector3.Dot(car.transform.forward, carToBallDirection) < 0)
+            {
+                desiredPosition = car.transform.position + car.transform.forward * distanceBehindCar + Vector3.up * heightAboveCar;
+            }
 
             Vector3 lookDirection = ball.transform.position - desiredPosition;
             Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
@@ -104,33 +108,6 @@ public class playerMovement : MonoBehaviour
 
         }
     }
-
-    // void faceCamBall() // ESTA SOLUÇAO FUNCIONA BEM
-    // {
-    //     float distanceBehindCar = 5f; 
-    //     float heightAboveCar = 2f; 
-
-    //     GameObject car = GameObject.FindGameObjectWithTag(gameObject.tag);
-    //     GameObject ball = GameObject.FindGameObjectWithTag("Ball");
-
-    //     if (car != null && ball != null)
-    //     {
-    //         Vector3 desiredPosition = car.transform.position - car.transform.forward * distanceBehindCar + Vector3.up * heightAboveCar;
-    //         Vector3 carToBallDirection = ball.transform.position - car.transform.position;
-
-    //         if (Vector3.Dot(car.transform.forward, carToBallDirection) < 0)
-    //         {
-    //             desiredPosition = car.transform.position + car.transform.forward * distanceBehindCar + Vector3.up * heightAboveCar;
-    //         }
-
-    //         Vector3 lookDirection = ball.transform.position - desiredPosition;
-    //         Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
-
-    //         playerCamera.transform.position = desiredPosition;
-    //         playerCamera.transform.rotation = targetRotation;
-
-    //     }
-    // }
 
 
 
